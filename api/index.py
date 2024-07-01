11922e4f151dec74e66ecb4f958acb25bb1ba4ad
harry <1577791638@qq.com>
2024-07-02 00:17:01 +0800

    fix: api not get data; closed(#1)

--- api/index.py
+++ api/index.py
@@ -7,10 +7,21 @@ import json
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 def getdata(name):
-    gitpage = requests.get("https://github.com/" + name)
+    headers = {
+        'Referer': 'https://github.com/' + name,
+        'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    gitpage = requests.get(f"https://github.com/{name}?action=show&controller=profiles&tab=contributions&user_id={name}", headers=headers)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
-    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute".*?role="tooltip">(.*?) contribution')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
